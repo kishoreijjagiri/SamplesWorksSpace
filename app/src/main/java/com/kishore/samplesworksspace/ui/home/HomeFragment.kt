1cbefeb962ae6bc6d6d640035b0d77bbcb9fb8d5
kishore.i <kishore.ijjagiri@halcyontek.com>
2020-02-18 17:14:05 +0530

    navigation components integrated

---
+++ app/src/main/java/com/kishore/samplesworksspace/ui/home/HomeFragment.kt
@@ -0,0 +1,49 @@
+package com.kishore.samplesworksspace.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import com.kishore.samplesworksspace.R
+import kotlinx.android.synthetic.main.content_main.*
+import kotlinx.android.synthetic.main.fragment_home.*
+
+class HomeFragment : Fragment() {
+
+    private lateinit var homeViewModel: HomeViewModel
+     lateinit var button :Button
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        homeViewModel =
+            ViewModelProviders.of(this).get(HomeViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        val textView: TextView = root.findViewById(R.id.text_home)
+        homeViewModel.text.observe(this, Observer {
+            textView.text = it
+        })
+          button =root.findViewById(R.id.button) as Button
+
+
+        return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        button.setOnClickListener {
+            view.findNavController(). navigate(R.id.action_nav_home_to_stepOneFragment)
+        }
+
+    }
+}
